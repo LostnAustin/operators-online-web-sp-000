394248569f8ac051a6971b39f3a281441eef5310
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-21 04:33:49 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 def unsafe?(speed)
-	 speed > 60? "True" : "False"
-		
+	 speed > 60? True : False
+
 	end
 
 
 def not_safe?(speed)
-	speed < 40 ? "True" : "False"
+	speed < 40 ? True : False
 
 	end
